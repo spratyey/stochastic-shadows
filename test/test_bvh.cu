e9111a51095416cc2aa52af49dc79bfdf2594ea2
Ishaan Shah <ishaan.n.shah@gmail.com>
2022-12-06 11:50:29 +0530

    Add benchmark for BVH and improve CMake stuff

--- test/test_bvh.cu
+++ test/test_bvh.cu
@@ -6,12 +6,74 @@
 #include "bvh.hpp"
 #include "constants.cuh"
 
+#ifdef DEBUG_BUILD
+#define print(bIdx, tIdx, ...) { if (blockIdx.x == bIdx && threadIdx.x == tIdx) { printf( __VA_ARGS__ ); } }
+#else
+#define print(bIdx, tIdx, ...)
+#endif
+
 using namespace owl;
 
 __constant__ int bvhHeight;
+__constant__ vec3f boundMin;
+__constant__ vec3f boundMax;
 
 __device__
-void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2i rand, int &selectedIdx, vec3f p) {
+void stochasticTraverseLBVH(LightBVH* bvh, vec2f rand, int& selectedIdx, vec3f p) {
+    selectedIdx = -1;
+
+    float r1 = rand.x;
+    float r2 = rand.y;
+
+    int nodeIdx = 0;
+    for (int i = 0; i < bvhHeight + 1; i++) {
+        LightBVH node = bvh[nodeIdx];
+
+        // If leaf
+        if (node.left == 0 && node.right == 0) {
+            if (node.primCount == 1) {
+                selectedIdx = node.primIdx;
+            }
+            else {
+                selectedIdx = node.primIdx + round(r1 * (node.primCount-1));
+            }
+
+            break;
+        }
+
+        LightBVH leftNode = bvh[node.left];
+        LightBVH rightNode = bvh[node.right];
+
+        float leftImp = leftNode.flux / pow(length(leftNode.aabbMid - p), 2.f);
+        float rightImp = rightNode.flux / pow(length(rightNode.aabbMid - p), 2.f);
+        float sum = leftImp + rightImp;
+
+        leftImp = leftImp / sum;
+        rightImp = rightImp / sum;
+
+        float eps = r2;
+        if (eps < 0.5) {
+            nodeIdx = node.left;
+        } else {
+            nodeIdx = node.right;
+        }
+
+        // Generate new random numbers
+        if (r1 < leftImp)
+            r1 = r1 / leftImp;
+        else
+            r1 = (r1 - leftImp) / rightImp;
+
+        // Generate new random numbers
+        if (r2 < leftImp)
+            r2 = r2 / leftImp;
+        else
+            r2 = (r2 - leftImp) / rightImp;
+    }
+}
+
+__device__
+void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2f rand, int &selectedIdx, vec3f p, int &checkHeight) {
     selectedIdx = -1;
 
     float r1 = rand.x;
@@ -20,6 +82,7 @@ void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2i rand, int &selec
     int nodeIdx = 0;
     bool prevNodeStatus = false;
     int toInsert = -1;
+    int toInsertHeight = -1;
 
     for (int i = 0; i <= bvhHeight; i++) {
         LightBVH node = bvh[nodeIdx];
@@ -36,6 +99,7 @@ void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2i rand, int &selec
       
             if (!prevNodeStatus) {
                 toInsert = nodeIdx;
+                toInsertHeight = i;
             }
             break;
         }
@@ -57,18 +121,21 @@ void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2i rand, int &selec
         }
   
         // Choose other node if one is taken
-        bool leftFull = set->exists(node.left);
-        bool rightFull = set->exists(node.right);
-        if ((!selected && leftFull) || (selected && rightFull)) {
-            selected = !selected;
+        if (i >= checkHeight) {
+            bool leftFull = set->exists(node.left);
+            bool rightFull = set->exists(node.right);
+            if ((!selected && leftFull) || (selected && rightFull)) {
+                selected = !selected;
+            }
+    
+            // Book-keeping
+            bool curStatus = leftFull || rightFull;
+            if (curStatus && !prevNodeStatus) {
+                toInsert = nodeIdx;
+                toInsertHeight = i;
+            }
+            prevNodeStatus = curStatus;
         }
-  
-        // Book-keeping
-        bool curStatus = leftFull || rightFull;
-        if (curStatus && !prevNodeStatus) {
-            toInsert = nodeIdx;
-        }
-        prevNodeStatus = curStatus;
   
         // Set selected nodeIdx
         nodeIdx = selected ? node.right : node.left;
@@ -88,21 +155,55 @@ void stochasticTraverseLBVHNoDup(LightBVH *bvh, Set *set, vec2i rand, int &selec
         }
     }
 
+    checkHeight = min(checkHeight, toInsertHeight - 1);
     set->insert(toInsert);
 }
 
 __global__
-void test_bvh(LightBVH *bvh) {
+void test_bvh_no_dup(LightBVH *bvh) {
     Set selectedSet;
     int selectedIdx;
+    int selectedLights[MAX_LTC_LIGHTS];
+    int selectedCount = 0;
+    LCGRand rng = get_rng(10007, make_uint2(blockIdx.x, threadIdx.x), make_uint2(gridDim.x, blockDim.x));
+    vec3f rand = vec3f(lcg_randomf(rng), lcg_randomf(rng), lcg_randomf(rng));
+    vec3f p = boundMin + rand * (boundMax - boundMin);
+    int checkHeight = bvhHeight;
     for (int i = 0; i < MAX_LTC_LIGHTS; i++) {
-        stochasticTraverseLBVHNoDup(bvh, &selectedSet, vec2i(0, 0), selectedIdx, vec3f(0));
+        stochasticTraverseLBVHNoDup(bvh, &selectedSet, vec2f(lcg_randomf(rng), lcg_randomf(rng)), selectedIdx, vec3f(0), checkHeight);
+        selectedLights[selectedCount++] = selectedIdx;
     }
+#ifdef DEBUG_BUILD
+    for (int i = 0; i < selectedCount; i++) {
+        print(0, 0, "%d ", selectedLights[i]);
+    }
+    printf(0, 0, "\n");
+#endif
+}
+
+__global__
+void test_bvh_dup(LightBVH *bvh) {
+    int selectedIdx;
+    int selectedLights[MAX_LTC_LIGHTS];
+    int selectedCount = 0;
+    LCGRand rng = get_rng(10007, make_uint2(blockIdx.x, threadIdx.x), make_uint2(gridDim.x, blockDim.x));
+    vec3f rand = vec3f(lcg_randomf(rng), lcg_randomf(rng), lcg_randomf(rng));
+    vec3f p = boundMin + rand * (boundMax - boundMin);
+    for (int i = 0; i < MAX_LTC_LIGHTS; i++) {
+        stochasticTraverseLBVH(bvh, vec2f(lcg_randomf(rng), lcg_randomf(rng)), selectedIdx, vec3f(0));
+        selectedLights[selectedCount++] = selectedIdx;
+    }
+#ifdef DEBUG_BUILD
+    for (int i = 0; i < selectedCount; i++) {
+        print(0, 0, "%d ", selectedLights[i]);
+    }
+    printf(0, 0, "\n");
+#endif
 }
 
 int main() {
     // Load the scene
-    std::string scenePath = "/home/aakashkt/ishaan/OptixRenderer/scenes/scene_configs/test_scene.json";
+    std::string scenePath = "/home/aakashkt/ishaan/OptixRenderer/scenes/scene_configs/bistro.json";
     Scene scene;
     bool success = parseScene(scenePath, scene);
 
@@ -166,6 +267,7 @@ int main() {
 
     LightBVH root;
     root.left = root.right = 0;
+    root.primIdx = 0;
     root.primCount = meshLightList.size();
     lightTlas.push_back(root);
 
@@ -176,12 +278,17 @@ int main() {
     std::cout << "Build BVH with " << lightTlas.size() << " nodes" << std::endl;
     std::cout << "Height of BVH " << lightTlasHeight << std::endl;
 
+    std::cout << "Scene Bounds (min): " << lightTlas[0].aabbMin.x << " " << lightTlas[0].aabbMin.y << " " << lightTlas[0].aabbMin.z << std::endl;
+    std::cout << "Scene Bounds (max): " << lightTlas[0].aabbMax.x << " " << lightTlas[0].aabbMax.y << " " << lightTlas[0].aabbMax.z << std::endl;
+
     // Create CUDA arrays to store BVH
     LightBVH* dLightTlas;
     cudaMalloc(&dLightTlas, lightTlas.size() * sizeof(LightBVH));
 
     // Copy BVH height to constant memory
     cudaMemcpyToSymbol(bvhHeight, &lightTlasHeight, sizeof(int));
+    cudaMemcpyToSymbol(boundMin, &lightTlas[0].aabbMin, sizeof(vec3f));
+    cudaMemcpyToSymbol(boundMax, &lightTlas[0].aabbMax, sizeof(vec3f));
 
     cudaMemcpy(dLightTlas, lightTlas.data(), lightTlas.size() * sizeof(LightBVH), cudaMemcpyHostToDevice);
 
@@ -190,19 +297,34 @@ int main() {
     int threadCount = 256;
     int blockSize = ceil((float)resolution / (float)threadCount);
 
-    std::cout << "Choosing " << MAX_LTC_LIGHTS << std::endl;
+    std::cout << "Choosing " << MAX_LTC_LIGHTS << " lights" << std::endl;
     std::cout << "Launching with " << blockSize << " blocks with " << threadCount << " threads per block" << std::endl;
 
     auto start = std::chrono::high_resolution_clock::now();
 
     // Launch kernel
-    test_bvh<<<blockSize,threadCount>>>(dLightTlas);
+    test_bvh_no_dup<<<blockSize,threadCount>>>(dLightTlas);
     
     // Wait for it to complete
     cudaDeviceSynchronize();
 
+
     auto finish = std::chrono::high_resolution_clock::now();
     auto milliseconds_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() / 1e6;
 
-    std::cout << "Time taken: " << milliseconds_taken << "ms" << std::endl;
+    std::cout << "Time taken for no duplicates: " << milliseconds_taken << "ms" << std::endl;
+
+    start = std::chrono::high_resolution_clock::now();
+
+    // Launch kernel
+    test_bvh_dup<<<blockSize,threadCount>>>(dLightTlas);
+    
+    // Wait for it to complete
+    cudaDeviceSynchronize();
+
+    finish = std::chrono::high_resolution_clock::now();
+
+    milliseconds_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(finish - start).count() / 1e6;
+
+    std::cout << "Time taken for duplicates: " << milliseconds_taken << "ms" << std::endl;
 }
